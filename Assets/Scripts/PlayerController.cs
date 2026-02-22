df278c0a08c4115e7aaf19b41d9dc7b75c18f6ed
Moosemello <neilf65@hotmail.com>
2026-02-21 21:28:26 -0500

    Minor Adjustments
    - Camera now turns with the player's rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,13 +14,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     
     private Vector2 moveInput;
-    private Vector3 lookInput;
     private Vector3 velocity;
+    private Vector2 camRotation;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
 
     }
 
@@ -38,17 +38,21 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("We are supposed to jump");
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            rb.AddForce(Vector3.up * velocity.y, ForceMode.Impulse);
         }
 
     }
 
     void Update()
     {
+        //inputs
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
         controller.Move(move * moveSpeed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        
+
     }
 
     private void FixedUpdate()
@@ -56,5 +60,36 @@ public class PlayerController : MonoBehaviour
         Vector3 move = cameraTransform.forward * moveInput.y + cameraTransform.right * moveInput.x;
         move.y = 0f;
         rb.AddForce(move.normalized * moveSpeed, ForceMode.VelocityChange);
+
+
+
     }
+
+    private void LateUpdate()
+    {
+        RotatePlayerToCameraForward();
+    }
+
+    private void RotatePlayerToCameraForward()
+    {
+        
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+
+        Vector3 forwardRelativeMovementVector = moveInput.y * cameraForward;
+        Vector3 rightRelativeMovementVector = moveInput.x * cameraRight;
+
+
+        Vector3 cameraRelativeMovement = forwardRelativeMovementVector + rightRelativeMovementVector;
+    
+        controller.Move(cameraRelativeMovement / 50f);
+
+    }
+
 }
